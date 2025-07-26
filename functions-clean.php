13032c427003c7c55768f4efe0a023c1ae1804e4
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-07-26 14:25:34 +0530

    f

--- functions-clean.php
+++ functions-clean.php
@@ -210,12 +210,6 @@ function blueprint_folder_scripts() {
     // Header improvements CSS
     wp_enqueue_style('blueprint-folder-header', get_template_directory_uri() . '/css/header-improvements.css', array('blueprint-folder-style'), '2.0.0');
     
-    // Header layout fixes CSS
-    wp_enqueue_style('blueprint-folder-header-fixes', get_template_directory_uri() . '/css/header-layout-fixes.css', array('blueprint-folder-header'), '2.0.1');
-    
-    // Debug CSS (remove in production)
-    wp_enqueue_style('blueprint-folder-debug', get_template_directory_uri() . '/debug-header.css', array('blueprint-folder-header-fixes'), '1.0.0');
-    
     // Bootstrap CSS (CDN)
     wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css', array(), '5.3.0');
     
