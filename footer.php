2fe3de68b6492b4ba1358f1b7ed16eff07470ea0
Umakant Yadav <umakant171991@gmail.com>
2025-07-26 12:18:29 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
             <div class="section-padding">
                 <div class="row">
                     <!-- Company Info -->
-                     testing
+                     testing new
                     <div class="col-lg-4 col-md-6 mb-4">
                         <h3 class="h5 text-accent mb-3"><?php bloginfo('name'); ?></h3>
                         <p class="text-light mb-3"><?php bloginfo('description'); ?></p>
